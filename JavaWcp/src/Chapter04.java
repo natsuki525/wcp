a42142cd7565e52e66d833f9cd3b0e70b0afa4f4
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-05-24 17:20:15 +0900

    [finish] 4章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "常見夏希";
+		System.out.println("私の名前は" + str1 + "です。");
+		str1 = "水滴石穿";
+		System.out.println("好きな言葉は" + str1 + "です。");
+		
+		final int LUCKY_NUMBER = 5;
+		System.out.println("ラッキーナンバーは" + LUCKY_NUMBER + "です。");
+	}
+}
