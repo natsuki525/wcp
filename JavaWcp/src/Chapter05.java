8d70808060c2c126221c56ec9833488eaeb0e647
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-05-25 22:44:56 +0900

    [finish] 5章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 913338484858784838L;
+		double double1 = 0.43;
+		boolean boolean1 = true;
+		
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 0.123F;
+		Integer integer1 = 525;
+		Character character1 = 'A';
+		
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+		
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
